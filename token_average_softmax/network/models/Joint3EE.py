0e376a58172b774280572edb86b448fc1781f3fc
LiXianyao <buptlixianyao@163.com>
2020-10-19 15:15:01 +0800

    1、变更为触发词分类后直接提供候选位置（取0分类打分最低的k个） 2、除了词本身的embedding，再拼上0号位的向量，然后加个FC降维

--- token_average_softmax/network/models/Joint3EE.py
+++ token_average_softmax/network/models/Joint3EE.py
@@ -10,8 +10,8 @@ from util import consts
 from .model import Model
 from .BertRepresentationLayer import BertRepresentationLayer
 from .ArgumentRoleClsBaseLayer import ArgumentRoleClsLayer
-from torchcrf import CRF
-
+#from torchcrf import CRF
+import numpy as np
 class Joint3EEModel(Model):
     def __init__(self, hyps, device, bert_model, *inputs, **kwargs):
         super(Joint3EEModel, self).__init__(*inputs, **kwargs)
@@ -25,14 +25,20 @@ class Joint3EEModel(Model):
         bert_output_size = self.bertConfig.hidden_size
         repr_len = bert_output_size
 
-        self.linear = nn.Linear(bert_output_size, hyps["trigger_label_weight"].size()[0], bias=True)
+        self.linear_t1 = nn.Linear(2 * bert_output_size, bert_output_size, bias=True)
+        self.linear_t2 = nn.Linear(bert_output_size, hyps["trigger_label_weight"].size()[0], bias=True)
+
+        self.linear_e1 = nn.Linear(2 * bert_output_size, bert_output_size, bias=True)
         self.dropout = nn.Dropout(hyps["dropout"])
-        init_linear_(self.linear)
+        init_linear_(self.linear_t1)
+        init_linear_(self.linear_t2)
+        init_linear_(self.linear_e1)
 
         if hyps["ed_cls_mode"] == 'crf':
-            self.event_CRF = CRF(len(hyps["trigger_label_weight"]))
+            pass
+            #self.event_CRF = CRF(len(hyps["trigger_label_weight"]))
         else:
-            self.loss = nn.CrossEntropyLoss(weight=hyps["trigger_label_weight"])
+            self.loss = nn.MultiMarginLoss(p=1, margin=5, weight=hyps["trigger_label_weight"])  # nn.CrossEntropyLoss(weight=hyps["trigger_label_weight"])
 
         self.eventDetectionLayer = EventDetectionLayer(hyps, trigger_repr_len=repr_len)
         self.entityMentionDetectionLayer = EntityMentionDetectionLayer(hyps, entity_reprensentation_len=repr_len)
@@ -68,7 +74,7 @@ class Joint3EEModel(Model):
         '''
         # get bert's embeddings
         attention_mask = self.get_mask_tensor(input_ids, input_len, 2)  # 对token序列的mask
-        trigger_hidden, entity_hidden = self.bertRepresentationLayer.forward(
+        trigger_hidden, entity_hidden, sequence_output = self.bertRepresentationLayer.forward(
             tokens=input_ids, segments=segments, token_mask=attention_mask,
             token_head_mask=token_head_mask
         )
@@ -76,20 +82,46 @@ class Joint3EEModel(Model):
         #print(w_len)
         trigger_labels_mask = self.get_mask_tensor(trigger_labels, trigger_labels_len, depth=2)  # 对trigger的mask
 
-        logits = self.linear(self.dropout(trigger_hidden))
+        sequence_output = torch.zeros([batch_size, max_len, feat_dim]).cuda() + sequence_output.view(batch_size, 1, feat_dim)
+        trigger_hidden = torch.cat([trigger_hidden, sequence_output], dim=-1)
+        print("sequence_output 's size is {}".format(
+            sequence_output.size())) if consts.ONECHANCE else None
+        print("trigger_hidden 's size is {}".format(
+            trigger_hidden.size())) if consts.ONECHANCE else None
+        trigger_hidden = self.dropout(trigger_hidden)
+        trigger_hidden = self.dropout(torch.tanh(self.linear_t1(trigger_hidden)))
+        logits = self.linear_t2(trigger_hidden)
+        
+        entity_hidden = self.dropout(entity_hidden)
+        entity_hidden = self.dropout(torch.tanh(self.linear_e1(entity_hidden)))
 
         if self.hyperparams["ed_cls_mode"] == "crf":
             loss_ed_cls = self.event_CRF.forward(logits, trigger_labels)
             ed_cls_label = self.event_CRF.decode(logits)
         else:
+            if self.training:
+                k = self.hyperparams["trigger_sampled_number"]
+                _, batch_mink_pos = logits.topk(k, 1, largest=False, sorted=True)
+                position_ = batch_mink_pos[:, :, 0].view(batch_size * k, 1)  # top k of 1 class in each batch, [batch * k]
+                seq_position = position_ // consts.TRIGGER_ANCHOR_NUM
+                anchor_position = position_ % consts.TRIGGER_ANCHOR_NUM
+
+                batch_matrix = np.array([range(batch_size)] * k).transpose()
+                batch_id = torch.LongTensor(batch_matrix).cuda().contiguous().view(batch_size * k, 1)
+                ed_can_idx = torch.cat([batch_id, seq_position, anchor_position], dim=1)
+                print("ed_can_idx 's size is {}".format(
+                    ed_can_idx.size())) if consts.ONECHANCE else None
+
             logits = logits.view([batch_size * max_len, -1])
             labels = trigger_anchor_type[:, :, 0].view(batch_size * max_len)
             loss_ed_cls = self.loss(logits, labels)
             ed_cls_label = torch.argmax(logits, 1).view([batch_size, max_len, 1])
+            if not self.training:
+                ed_can_idx = torch.nonzero(ed_cls_label > 0)
 
         #(batch_size, seq_len, out) -》( batch, seqlen,  anchor_num, 2)
-        loss_emd, emd_det_label, loss_emd_cls, emd_cls_label = self.entityMentionDetectionLayer.forward(
-                seq_mask=trigger_labels_mask, cnn_representation=None, word_representation=self.dropout(entity_hidden),
+        loss_emd, emd_det_label, emd_candidates_idx, loss_emd_cls, emd_cls_label = self.entityMentionDetectionLayer.forward(
+                seq_mask=trigger_labels_mask, cnn_representation=None, word_representation=entity_hidden,
                 entity_anchor_labels=entity_anchor_labels, entity_anchor_loc=entity_anchor_loc,
                 entity_anchor_type=entity_anchor_type)
 
@@ -101,10 +133,12 @@ class Joint3EEModel(Model):
             seq_mask=trigger_labels_mask,
             trigger_anchor_loc=trigger_anchor_loc, trigger_anchor_type=trigger_anchor_type,
             trigger_representation=trigger_hidden, trigger_predict_labels=ed_cls_label,
+            trigger_can_idx=ed_can_idx,
 
             entity_anchor_loc=entity_anchor_loc, entity_anchor_type=entity_anchor_labels,
             #entity_anchor_type=entity_anchor_type,
             entity_predict_labels=emd_det_label, entity_representation=entity_hidden,
+            entity_can_idx=emd_candidates_idx,
 
             trigger_label_i2s=trigger_label_i2s, trigger_label_s2i=trigger_label_s2i, entity_label_i2s=entity_label_i2s,
             batch_golden_events=batch_golden_events)
